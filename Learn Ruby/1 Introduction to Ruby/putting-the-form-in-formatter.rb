6d48607d6a5b5693f549215fdafea55d566b1129
dhalv001 <daviqd.halvorsen@gmail.com>
2017-08-23 19:55:22 -0700

    Update putting-the-form-in-formatter.rb

--- Learn Ruby/1 Introduction to Ruby/putting-the-form-in-formatter.rb
+++ Learn Ruby/1 Introduction to Ruby/putting-the-form-in-formatter.rb
@@ -1,7 +1,13 @@
 =begin
+This program is the solution to Unit 1 of the Learn Ruby course, which is part of the Codecademy Full Stack Web Developer Track. 
 This program collects name & hometown information from the user with the gets.chomp command.
 It corrects for improper capitalization usage by the user with the upcase and capitalize methods.
-I go into more detail about how these methods work in the README.md
+Print and puts are similar methods. Print does not add a new line. The difference between the two is that puts adds a new line AND does not list "nil" values. 
+Both need string values in quotes. Chomp removes the enter character from the string's end. Gets would pick up the final enter without the chomp command ... 
+it'd be listed as having "\n" in addition to the inputted text without the chomp. Capitalize and upper have similar results. 
+Capitalize will capitalize only the first letter. Call this command by adding ".capitalize". The upcase method would capitalize the entered string ... 
+that's what we do with the capitailze state part. Expressions inside of #{} are evaluate before being included in the string. 
+That's how the final output is created.
 =end
 
 print "What's your first name? "
