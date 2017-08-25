b957e390d5da5e9f40d22e25828594ed6b1ee323
dhalv001 <daviqd.halvorsen@gmail.com>
2017-08-24 23:15:55 -0700

    Create a-night-at-the-movies.rb

---
+++ Learn Ruby/a-night-at-the-movies.rb
@@ -0,0 +1,59 @@
+=begin
+The "A Night at the Movies" project is the concluding exercise for the Learn Ruby unit 6 track called "Hashes and Symbols."
+This work is part of Codecademy's Full Stack Web Developer Program that I am working on.
+This program maintains a list of movies in a hash, with movie ratings, and allows for new movies to be added, current movies 
+to be deleted, current movies to get updated ratings and for all movies in the hash to be displayed.
+=end
+
+movies = {
+  Memento: 3,
+  Primer: 4,
+  Ishtar: 1
+}
+
+puts "What would you like to do?"
+puts "-- Type 'add' to add a movie."
+puts "-- Type 'update' to update a movie."
+puts "-- Type 'display' to display all movies."
+puts "-- Type 'delete' to delete a movie."
+
+choice = gets.chomp.downcase
+case choice
+when 'add'
+  puts "What movie do you want to add?"
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "What's the rating? (Type a number 0 to 4.)"
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+    puts "#{title} has been added with a rating of #{rating}."
+  else
+    puts "That movie already exists! Its rating is #{movies[title.to_sym]}."
+  end
+when 'update'
+  puts "What movie do you want to update?"
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "Movie not found!"
+  else
+    puts "What's the new rating? (Type a number 0 to 4.)"
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+    puts "#{title} has been updated with new rating of #{rating}."
+  end
+when 'display'
+  movies.each do |movie, rating|
+    puts "#{movie}: #{rating}"
+  end
+when 'delete'
+  puts "What movie do you want to delete?"
+  title = gets.chomp
+  if movies[title.to_sym].nil?
+    puts "Movie not found!"
+  else
+    movies.delete(title.to_sym)
+    puts "#{title} has been removed."
+  end
+else
+  puts "Sorry, I didn't understand you."
+end
