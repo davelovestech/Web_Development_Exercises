d419653678113ca295fac9cda1f9e71c7caefee4
David <daviqd.halvorsen@gmail.com>
2017-08-29 00:18:57 -0700

    Create posts_controller.rb

---
+++ Learn Ruby on Rails/2 Saving Data/Threadly/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+    def index
+        @new_post = Post.new 
+        @all_posts = Post.order(created_at: :desc).all 
+    end
+    def create
+        @new_post = Post.new(post_params) 
+        if @new_post.save 
+          redirect_to root_path 
+        else 
+          render 'index' 
+        end 		
+    end
+private
+    def post_params
+      params.require(:post).permit(:comment)
+    end
+end
