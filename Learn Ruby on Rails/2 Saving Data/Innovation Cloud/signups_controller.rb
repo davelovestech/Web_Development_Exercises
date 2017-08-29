32cfe0617194425e5ca9369761bffc3e84f766d8
David <daviqd.halvorsen@gmail.com>
2017-08-28 22:26:49 -0700

    Create signups_controller.rb

---
+++ Learn Ruby on Rails/2 Saving Data/Innovation Cloud/signups_controller.rb
@@ -0,0 +1,17 @@
+class SignupsController < ApplicationController
+    def new 
+        @signup = Signup.new 
+    end
+    def create 
+        @signup = Signup.new(signup_params) 
+        if @signup.save 
+          redirect_to '/thanks' 
+        else 
+          render 'new' 
+        end 
+    end	
+    private
+    def signup_params
+        params.require(:signup).permit(:email)
+    end
+end
