6c4804d02a99c9fe9abc66e4d7c55c4ac89b7dae
David <daviqd.halvorsen@gmail.com>
2017-08-31 01:42:55 -0700

    Create 20170831065650_create_reviews.rb

---
+++ Learn Ruby on Rails/Associations I/Bookmarks/20170831065650_create_reviews.rb
@@ -0,0 +1,10 @@
+class CreateReviews < ActiveRecord::Migration[5.1]
+  def change
+    create_table :reviews do |t|
+      t.string :author
+      t.string :comment
+      t.references :book  
+      t.timestamps
+    end
+  end
+end
