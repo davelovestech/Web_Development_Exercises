902e6cc4bad44aa5f6cb55d6380d637804b3c864
David <daviqd.halvorsen@gmail.com>
2017-08-31 01:37:58 -0700

    Create books_controller.rb

---
+++ Learn Ruby on Rails/Associations I/Bookmarks/books_controller.rb
@@ -0,0 +1,9 @@
+class BooksController < ApplicationController
+    def index
+        @books = Book.all 
+    end
+    def show 
+        @book = Book.find(params[:id]) 
+        @reviews = @book.reviews 
+    end
+end
