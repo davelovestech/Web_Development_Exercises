1f41c49c7d92636be3e43da4caa910c035906912
David <daviqd.halvorsen@gmail.com>
2017-08-31 01:40:14 -0700

    Create 20170831065612_create_books.rb

---
+++ Learn Ruby on Rails/Associations I/Bookmarks/20170831065612_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :description
+      t.string :publisher
+      t.integer :weeks_on_list
+      t.integer :rank_this_week
+      t.timestamps
+    end
+  end
+end
