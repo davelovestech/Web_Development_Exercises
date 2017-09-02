309c548e1fc406444a2ae3c8266e222aab371e90
David <daviqd.halvorsen@gmail.com>
2017-09-02 15:49:24 -0700

    Create albums_controller.rb

---
+++ Learn Ruby on Rails/Associations I/Bass Music/albums_controller.rb
@@ -0,0 +1,9 @@
+class AlbumsController < ApplicationController
+    def index
+        @albums = Album.all
+    end
+    def show 
+        @album = Album.find(params[:id]) 
+        @tracks = @album.tracks 
+    end
+end
