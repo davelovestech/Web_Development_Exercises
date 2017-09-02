4a528b6411429e2f209e8b24f1c06365588fd6ed
David <daviqd.halvorsen@gmail.com>
2017-09-02 15:53:08 -0700

    Create 20170902203955_create_albums.rb

---
+++ Learn Ruby on Rails/Associations I/Bass Music/20170902203955_create_albums.rb
@@ -0,0 +1,10 @@
+class CreateAlbums < ActiveRecord::Migration[5.1]
+  def change
+    create_table :albums do |t|
+      t.string :cover
+      t.string :title
+      t.string :artist
+      t.timestamps
+    end
+  end
+end
