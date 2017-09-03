8e7705e06779d5d0ab584483e2760d109a40e1a1
David <daviqd.halvorsen@gmail.com>
2017-09-02 20:12:02 -0700

    Create actors_controller.rb

---
+++ Learn Ruby on Rails/Associations II/actors_controller.rb
@@ -0,0 +1,9 @@
+class ActorsController < ApplicationController
+    def index
+		@actors = Actor.all
+    end
+    def show
+        @actor = Actor.find(params[:id])
+        @movies = @actor.movies
+    end
+end
