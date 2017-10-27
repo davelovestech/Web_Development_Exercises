434984ed30f319b8462d7ffd1f3aef4d2f63c7f7
David Halvorsen <daviqd.halvorsen@gmail.com>
2017-10-27 16:51:41 -0700

    Create messageMixer.js
    I need to finish adding comments for this.

---
+++ Introduction to Javascript/Message Mixer/messageMixer.js
@@ -0,0 +1,83 @@
+//let MessageMixer = {};
+
+function countCharacter(inputString, inputCharacter) {
+  let count = 0;
+  let string = inputString.toLowerCase();
+  let character = inputCharacter.toLowerCase();
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] === character) {
+         count++;
+      }
+    }
+  return count; 
+};
+
+function capitalizeFirstCharacterOfWords(string) {
+  let arr = string.split(" ");  
+    for (let i = 0; i < arr.length; i++) {  
+      let word = arr[i];
+        arr[i] = word[0].toUpperCase() + word.substring(1); 
+    }
+  return arr.join(" "); 
+};
+
+
+function reverseWord(word) {
+  return word.split("").reverse().join("");
+};
+
+function reverseAllWords(sentence) {
+  let words = sentence.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      words[i] = reverseWord(words[i]);
+    }
+   return words.join(" ");
+};
+
+
+function replaceFirstOccurence(string, toBeReplaced, replaceWith) {
+  return string.replace(toBeReplaced, replaceWith);
+};
+
+
+function replaceAllOccurrences(string, toBeReplaced, replaceWith) {
+  return string.split(toBeReplaced).join(replaceWith);
+};
+
+function encode(string) {
+  let replacementObject = { "a": "@", "s": "$", "i": "!", "o":"0" };
+    for (let key in replacementObject) {
+      string = replaceAllOccurrences(string, key, replacementObject[key]); 
+    }	
+    return string;
+};
+function palindrome(string) {
+  return string + reverseWord(string);
+};
+function pigLatin(sentence, character) {
+  return sentence.split(" ").join(character + " ");
+};
+
+
+//export default MessageMixer;
+export { countCharacter, capitalizeFirstCharacterOfWords, reverseWord, reverseAllWords, replaceFirstOccurence, replaceAllOccurrences, encode, palindrome, pigLatin };
+
+
+
+
+
+
+//module.exports = MessageMixer;
+
+/*
+function displayMessage() {
+  console.log(countCharacter("What is the color of the sky?", "t"));
+  console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
+  console.log(reverseWord("What is the color of the sky?"));
+  console.log(reverseAllWords("What is the color of the sky?"));
+  console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
+  
+  console.log(encode("What is the color of the sky?"));
+}
+displayMessage();
+*/
