8a43e42bc1835d69c1981d10df775c25a3a1ab4e
David Halvorsen <daviqd.halvorsen@gmail.com>
2017-11-01 16:45:18 -0700

    Update main.js

--- Introduction to Javascript/Newsworthy/main.js
+++ Introduction to Javascript/Newsworthy/main.js
@@ -1,74 +1,80 @@
-const Twitter = {};
+// Page Elements
 
-const oauthConsumerKey = '8woTAssgST6YdVeH1Axso9KPt'; // Generated from apps.twitter.com
-const oauthConsumerSecret = 'yZcJddiwNOsuiamDaSgHxt7O9lb2RCiQJVdR5nhTiMa5Qff95i'; // Generated from Keys and Access Tokens on apps.twitter.com
-const oauthToken = '818889528-6z0YKLtdSvBPDJCFCxxW0niws2VrAPbnLcKeAb4Y'; // Generated from Keys and Access Tokens on apps.twitter.com
-const oauthTokenSecret = '86ZncUp5kqZdATGgxFtynTkOK2GGFT65jskg7tvsQS2Dw'; // Generated from Keys and Access Tokens on apps.twitter.com
-const oauthSignatureMethod = 'HMAC-SHA1';
-const oauthVersion = '1.0';
-const baseUrl = 'https://api.twitter.com/1.1/statuses/update.json';
+const engadget = document.getElementById('engadget');
+const recode = document.getElementById('recode');
+const nextWeb = document.getElementById('nextWeb');
+const main = document.getElementsByTagName('main')[0];
 
-// Post Status Function
-Twitter.postStatus = async (status) => {
-  let response = await fetch(`https://cors-anywhere.herokuapp.com/${baseUrl}?status=${encodeData(status)}`, {
-    method: 'POST',
-    headers: {
-  Authorization: Twitter.generateAuthorizationHeader(status)
-}	
-  });
+// News API Data
+
+const apiKey = '73b36fbee62b48f6881633408040acfb';
+const engadgetUrl = 'https://newsapi.org/v1/articles?source=engadget&sortBy=top&apiKey=73b36fbee62b48f6881633408040acfb';
+const recodeUrl = 'https://newsapi.org/v1/articles?source=recode&sortBy=top&apiKey=73b36fbee62b48f6881633408040acfb';
+const nextWebUrl = 'https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=73b36fbee62b48f6881633408040acfb';
+
+// Request News Function
+async function getNews(url) {
+  let response = await fetch(url);
   let jsonResponse = await response.json();
+  let articlesArray = jsonResponse.articles.slice(0,5)
   console.log(jsonResponse);
-  return jsonResponse;
-};
-// Helper Functions
+  console.log(articlesArray);
+  return articlesArray;
+  
+}
+// Render Function
 
-Twitter.generateAuthorizationHeader = (status) => {
-  const oauthNonce = generateNonce();
-  const oauthTimestamp = Math.floor((new Date()).getTime() / 1000);
-  const oauthSignature = generatePostSignature(status, baseUrl, oauthNonce, oauthTimestamp);
+function renderNews(articles) {
+  articles.map((article, index) => {
+    let articleRow =
+      '<div class="articlerow">' +
+      ' <div class="article">' +
+      '   <h2 class="title">' + article.title + '</h2>' +
+      '   <h3>By ' + article.author + '</h3>' +
+      '   <p> ' + article.description + '</p>' +
+      '   <a href="' + article.url + '" target="_blank" class="readmore"><p>Read More</p></a>' +
+      ' </div>' +
+      ' <div class="share">' +
+      '   <img class="storyimage" src="' + article.urlToImage + '" />' +
+      '   <a href="https://twitter.com/davehalvorsen" target="_blank"><button type="button" class="tweet" id="tweet ' + index + '">' +
+      '   <i class="fa fa-twitter" aria-hidden="true"></i>Tweet This</button></a>' +
+      ' </div>' +
+      '</div>';
 
-  let authorizationHeader = `Oauth oauth_consumer_key="${encodeData(oauthConsumerKey)}", oauth_nonce="${encodeData(oauthNonce)}", oauth_signature="${encodeData(oauthSignature)}", oauth_signature_method="${encodeData(oauthSignatureMethod)}", oauth_timestamp="${encodeData(oauthTimestamp)}", oauth_token="${encodeData(oauthToken)}", oauth_version="${encodeData(oauthVersion)}"`;
+    main.innerHTML += articleRow;
+  });
+  return articles;
+}
 
-  return authorizationHeader;
-};
+// Post Tweet Function
 
-NONCE_CHARS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-  'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
-  'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-  'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3',
-  '4', '5', '6', '7', '8', '9'
-];
-
-const generateNonce = () => {
-  var result = [];
-  var chars = NONCE_CHARS;
-  var char_pos;
-  var nonce_chars_length = chars.length;
-
-  for (var i = 0; i < 32; i++) {
-    char_pos = Math.floor(Math.random() * nonce_chars_length);
-    result[i] = chars[char_pos];
+function sendTweets(newsObjects) {
+  let tweetButtons = document.getElementsByClassName('tweet');
+  for (let i = 0; i < tweetButtons.length; i++) {
+    tweetButtons[i].addEventListener('click', function() {
+      // Call Post Status function here
+      Twitter.postStatus(newsObjects[i].url);
+      tweetButtons[i].innerHTML = "Tweeted";
+    }, false);
   }
-  return result.join('');
-};
+}
 
-const generatePostSignature = (status, url, oauthNonce, oauthTimestamp) => {
-  const parameterString = `oauth_consumer_key=${encodeData(oauthConsumerKey)}&oauth_nonce=${encodeData(oauthNonce)}&oauth_signature_method=HMAC-SHA1&oauth_timestamp=${encodeData(oauthTimestamp)}&oauth_token=${encodeData(oauthToken)}&oauth_version=1.0&status=${encodeData(status)}`;
-  const signatureBase = `POST&${encodeData(url)}&${encodeData(parameterString)}`;
-  const signingKey = `${encodeData(oauthConsumerSecret)}&${encodeData(oauthTokenSecret)}`;
+// Button Event Listeners
 
-  return HMACSHA1(signingKey, signatureBase);
-};
+engadget.addEventListener('click', function() {
+  main.innerHTML = ' ';
+  // Call getNews() here
+  getNews(engadgetUrl).then(articlesArray => renderNews(articlesArray)).then(articles => sendTweets(articles));
+}, false);
 
-const encodeData = (toEncode) => {
-  if (toEncode == null || toEncode == "") return "";
-  else {
-    var result = encodeURIComponent(toEncode);
-    // Fix the mismatch between OAuth's  RFC3986's and Javascript's beliefs in what is right and wrong ;)
-    return result.replace(/\!/g, "%21")
-      .replace(/\'/g, "%27")
-      .replace(/\(/g, "%28")
-      .replace(/\)/g, "%29")
-      .replace(/\*/g, "%2A");
-  }
-};
+recode.addEventListener('click', function() {
+  main.innerHTML = ' ';
+  // Call getNews() here
+  getNews(recodeUrl).then(articlesArray => renderNews(articlesArray)).then(articles => sendTweets(articles));
+}, false);
+
+nextWeb.addEventListener('click', function() {
+  main.innerHTML = ' ';
+  // Call getNews() here
+  getNews(nextWebUrl).then(articlesArray => renderNews(articlesArray)).then(articles => sendTweets(articles));
+}, false);
