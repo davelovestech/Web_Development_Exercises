11ba5a6feb7393ad2eba7c55590c49439a2cc89a
David Halvorsen <daviqd.halvorsen@gmail.com>
2017-11-09 18:24:18 -0800

    Create Car Loan Calculator.java

---
+++ Learn Java/Car Loan Calculator.java
@@ -0,0 +1,28 @@
+/*
+This freeform exercise is part of the Codecademy
+Learn Java track. I use if / else if / else syntax to 
+calculate monthlyPayment for a car loan.
+*/
+public class CarLoan {
+	public static void main(String[] args) {
+    int carLoan = 10000;
+    int loanLength = 3;
+    int interestRate = 5;
+    int downPayment = 2000;
+    if (loanLength <= 0) {
+      System.out.println("Erro! You must take out a valid car loan.");
+    } else if (downPayment >= carLoan) {
+      System.out.println("The car can be paid in full.");
+    } else {
+      int remainingBalance = carLoan - downPayment;
+      int months = loanLength * 12;
+      int monthlyBalance = remainingBalance / months;
+    	int interest = (monthlyBalance * interestRate)/100;
+      int monthlyPayment = monthlyBalance + interest;
+      System.out.println(monthlyPayment);
+    }
+
+	
+
+	}
+}
