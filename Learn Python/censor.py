16ddf6ac365ac7d00a8b46ea46bce1eab5aaedd8
David Halvorsen <daviqd.halvorsen@gmail.com>
2017-11-15 14:41:15 -0800

    Create censor.py

---
+++ Learn Python/censor.py
@@ -0,0 +1,12 @@
+def censor(text, word):
+    words = text.split()
+    result = ''
+    stars = '*' * len(word)
+    count = 0
+    for i in words:
+        if i == word:
+            words[count] = stars
+        count += 1
+    result =' '.join(words)
+
+    return result
