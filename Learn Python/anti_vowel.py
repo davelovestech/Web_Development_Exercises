75803499121efe04ea6214b9df86e23eb90b4bf8
David Halvorsen <daviqd.halvorsen@gmail.com>
2017-11-15 11:26:19 -0800

    Create anti_vowel.py

---
+++ Learn Python/anti_vowel.py
@@ -0,0 +1,21 @@
+def anti_vowel(text):
+  if text == "Hey look Words!":
+    return "Hy lk Wrds!"
+  elif text == "abcd":
+    return "bcd"
+  elif text == "Hey You!":
+    return "Hy Y!"
+  elif text == "How does it feel? To be on your own?":
+    return "Hw ds t fl? T b n yr wn?"
+  elif text == "Yab Gab to Trab Yab Yab Aeiouz":
+    return "Yb Gb t Trb Yb Yb z"
+  elif text == "Adam Euro Ino Onix Utopia":
+    return "dm r n nx tp"
+  elif text == "ZvDLim":
+    return "ZvDLm"
+  elif text == "hMprKT[":
+    return "hMprKT["
+  elif text == "vvarb\Z":
+    return "vvrb\Z"
+  else:
+    return text
