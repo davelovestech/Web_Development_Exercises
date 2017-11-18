6568fc368f25cbf8a82905f3f882c05af243e527
David Halvorsen <daviqd.halvorsen@gmail.com>
2017-11-18 15:27:21 -0800

    Update main.js

--- Introduction to Javascript/Newsworthy/main.js
+++ Introduction to Javascript/Newsworthy/main.js
@@ -1,18 +1,29 @@
+/*
+This Codecademy project is called Newsworthy. It's an instruction on using 
+APIs. This project uses APIs from NewsAPI.org and Twitter to fetch, and then
+display, news information from three different news sources. It was my first
+time signing up for developer accounts for the APIs that'll be needed. It was
+very exciting to see this project post to my personal Twitter account! :D
+*/
 // Page Elements
-
+//These constants are for selecting the index.html file by different ids.
 const engadget = document.getElementById('engadget');
 const recode = document.getElementById('recode');
 const nextWeb = document.getElementById('nextWeb');
 const main = document.getElementsByTagName('main')[0];
 
 // News API Data
-
+//Here are all the items required to make API calls. The most important
+//component is my personal API key.
 const apiKey = '73b36fbee62b48f6881633408040acfb';
 const engadgetUrl = 'https://newsapi.org/v1/articles?source=engadget&sortBy=top&apiKey=73b36fbee62b48f6881633408040acfb';
 const recodeUrl = 'https://newsapi.org/v1/articles?source=recode&sortBy=top&apiKey=73b36fbee62b48f6881633408040acfb';
 const nextWebUrl = 'https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=73b36fbee62b48f6881633408040acfb';
 
 // Request News Function
+/*
+This function grabs 
+*/
 async function getNews(url) {
   let response = await fetch(url);
   let jsonResponse = await response.json();
@@ -23,7 +34,9 @@ async function getNews(url) {
   
 }
 // Render Function
+/*
 
+*/
 function renderNews(articles) {
   articles.map((article, index) => {
     let articleRow =
@@ -47,7 +60,9 @@ function renderNews(articles) {
 }
 
 // Post Tweet Function
+/*
 
+*/
 function sendTweets(newsObjects) {
   let tweetButtons = document.getElementsByClassName('tweet');
   for (let i = 0; i < tweetButtons.length; i++) {
@@ -60,7 +75,15 @@ function sendTweets(newsObjects) {
 }
 
 // Button Event Listeners
-
+/*
+There are three different calls on three different news sites. .addEventListener checks for mouse 'click's on the specified constant
+document.getElementById that it is called upon (see ~line 11 of main.js).
+It then runs the function within the curly brackets. The getNews function
+is called with the selected news website's url, which returns the 5 article
+array. Then, that's passed into the renderNews function, which creates
+the HTML layout. That returns the structured articles, which are then
+passed as a parameter into the sendTweets function. 
+*/
 engadget.addEventListener('click', function() {
   main.innerHTML = ' ';
   // Call getNews() here
