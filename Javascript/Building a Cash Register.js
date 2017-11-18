7b49b4740c23600000232aeb0389596359b56c45
David Halvorsen <daviqd.halvorsen@gmail.com>
2017-11-17 20:35:31 -0800

    Update Building a Cash Register.js

--- Javascript/Building a Cash Register.js
+++ Javascript/Building a Cash Register.js
@@ -1,7 +1,9 @@
 /*
 This program is called Building a Cash Register. It allows for adding new StaffMember objects
-with defined names & discountPercent instances. 
-
+with defined names & discountPercent instances. Then, there's a cashRegister variable that 
+contains an object with the ability to add new items by scanning, void the last transaction, 
+and apply staff discount to drop the bill and, obviously, also the ability the print the 
+cost of each item. 
 */
 /*
 The function StaffMember accepts inputs of name & discountPercent and creates new StaffMember 
@@ -15,15 +17,18 @@ function StaffMember(name,discountPercent){
 
 var sally = new StaffMember("Sally",5);
 var bob = new StaffMember("Bob",10);
+/*
 
+*/
 // Here is 'me' with a staff discount of 20%
 var me = new StaffMember("David", 20);
-//This is the cashRegister object with starting 0 values for total & lastTransaction amount.
-//It has the ability to add a new item with the add instance. This only accepts itemCost & it
-//also keeps track of the last item cost for the voidLastTransaction object instance. The scan
-//function uses a swith case to screen for 1 of 4 choices. The voidLastTransaction function subtracts
-//the last transcation amount from the total. The applyStaffDiscount function drops the total by
-//employee's discountPercent. 
+/*
+This is the main part of this program. It starts the cash register out at a total of 0
+and a lastTransaction of 0. The add intance adds the new item price to the total AND
+it saves that as the lastTransaction, so that a later voidLastTransaction instance
+can run. Additionally, there's the staffDiscount instance of the object that can
+drop the price for employees. 
+*/
 var cashRegister = {
     total:0,
     lastTransactionAmount: 0,
@@ -49,13 +54,19 @@ var cashRegister = {
         this.total = this.total - (this.total * 0.2);
     }    
 };
-
+//Here, the program adds three different items; the third item, magazines, is added 3 times.
 cashRegister.scan('eggs',1);
 cashRegister.scan('milk',1);
 cashRegister.scan('magazine',3);
-// Apply your staff discount by passing the 'me' object 
-// to applyStaffDiscount
+//Here's the result of the total instance of the object AFTER adding those previous items.
+console.log("The result of calling the cashRegister.scan function on 1 'eggs', 1 'milk' & 3 'magazines' is a bill of: $" + cashRegister.total.toFixed(2))
+//This voids the last transaction (of the three magazines).
+cashRegister.voidLastTransaction()
+console.log("\nThe result of calling the 'voidLastTransaction' command, to get rid of the 3 magazines is a bill of: $" + cashRegister.total.toFixed(2))
+//Here, it calls the discount for the me variable & then calls the total again. 
 cashRegister.applyStaffDiscount(me);
-
-// Show the total bill
-console.log('Your bill is '+cashRegister.total.toFixed(2));
+console.log("\nThe result of calling the 'voidLastTransaction' command, to get rid of the 3 magazines, AND THEN also applying the staff count for myself (the me variable) is a bill of: $" + cashRegister.total.toFixed(2))
+//I wonder ... can you add multiple people's discounts together? Here, it adds the discount for bob.
+//I don't see why not, BUT that's an issue with the program because it doesn't reflect the usual scenario.
+cashRegister.applyStaffDiscount(bob);
+console.log("\nThe result of calling the 'voidLastTransaction' command, to get rid of the 3 magazines, AND THEN also applying the staff count for myself (the me variable) AND ALSO the staff discount for bob results in a bill of: $" + cashRegister.total.toFixed(2))
