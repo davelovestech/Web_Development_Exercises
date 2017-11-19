6a5bc577490fc6c60f39a63c02170a73714e16c1
David Halvorsen <daviqd.halvorsen@gmail.com>
2017-11-18 21:02:18 -0800

    Update Mad Libs.py

--- Learn Python/Mad Libs.py
+++ Learn Python/Mad Libs.py
@@ -1,3 +1,9 @@
+"""This program is called Mad Libs. It is from the Codecademy
+Learn Python track and can be found in chapter 2: Strings and Console Output. This program is a virtual edition of the 
+Mad Libs game that children play.
+"""
+#First the user is asked for a series of entries that will 
+#be plugged into the story at the end of the program. 
 print "Mad Libs has started!"
 name = raw_input('What is your name?')
 first_adj = raw_input('Enter an adjedctive :')
@@ -20,19 +26,9 @@ A_dessert = raw_input('Enter a dessert')
 A_year = raw_input('Enter a year')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 #The template for the story
 STORY = "This morning I woke up and felt %s because %s was going to finally %s over the big %s %s. On the other side of the %s were many %ss protesting to keep %s in stores. The crowd began to %s to the rythym of the %s, which made all of the %ss very %s. %s tried to %s into the sewers and found %s rats. Needing help, %s quickly called %s. %s appeared and saved %s by flying to %s and dropping %s into a puddle of %s. %s then fell asleep and woke up in the year %s, in a world where %ss ruled the world %s %s."
+
+#Here string interpolation is used to print the story with
+#all of the user-selected Mad Libs within it. 
 print STORY % (first_adj, name, first_verb, second_adj, first_noun, second_noun, An_animal, A_food, second_verb, third_noun, A_fruit, third_adj, name, third_verb, A_fruit, third_adj, name, third_verb, A_number, name, A_superhero_name, A_superhero_name, name, A_dessert, name, A_year, fourth_noun)     
