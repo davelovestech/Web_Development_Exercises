959e577d4dc6821e109fccf16b6916d3a054226f
David Halvorsen <daviqd.halvorsen@gmail.com>
2017-11-18 23:17:38 -0800

    Delete reverse.py

--- Learn Python/reverse.py
+++
@@ -1,8 +0,0 @@
-def reverse(text):
-  reversed = ""
-  length = len(text)
-  counter = length - 1
-  while counter >= 0:
-    reversed = reversed + (text[counter])
-    counter -= 1
-  return reversed
