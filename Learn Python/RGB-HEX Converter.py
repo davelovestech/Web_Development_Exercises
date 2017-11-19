5eb189e91d436cec24d8fd1ee2b59598df29be93
David Halvorsen <daviqd.halvorsen@gmail.com>
2017-11-18 23:05:04 -0800

    Update RGB-HEX Converter.py

--- Learn Python/RGB-HEX Converter.py
+++ Learn Python/RGB-HEX Converter.py
@@ -1,3 +1,13 @@
+"""
+This Python program is called RGB-HEX Converter. It's tutorial
+can be found at Codecademy in chapter 10: Advanced Topics in
+Python. This program can convert RGB values to HEX and also 
+HEX to RGB values.
+"""
+#rgb_hex uses raw_input to get RGB inputs for red, blue & also
+#green. Logic statements are uses to limit inputs to within 0-
+#255. Bitwise operators are used to print the HEX value of the
+#input. 
 def rgb_hex():
   invalid_msg = "ERROR! Your values are invalid."
   red = int(raw_input("Enter a value for red (R): "))
@@ -14,6 +24,9 @@ def rgb_hex():
     return
   val = (red << 16) + (green << 8) + blue
   print "%s" % (hex(val)[2:]).upper()
+#This function translate hex into rgb. raw_input is used to ask
+#the user for a hex_val. Bitwise operators are used to 
+#translate the hex value into rgb. 
 def hex_rgb():
   hex_val = raw_input("Enter a 6-digit hexadecimal value: ")
   if len(hex_val) != 6:
@@ -28,6 +41,9 @@ def hex_rgb():
   hex_val = hex_val >> 8
   red = hex_val % two_hex_digits
   print "Red: %s Green: %s Blue: %s" % (red, green, blue)
+#A while loop is used to keep the function going. The user is 
+#given the choice of converting from RGB to HEX OR converting 
+#HEX into RGB AND also there's the option to quit. 
 def convert():
   while True:
     option = raw_input("Enter 1 to convert RGB to HEX. Enter 2 to convert HEX to RGB. Enter X to Exit: ")
