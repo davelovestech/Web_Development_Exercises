25b168e9ca5c3920c4f7d606419e6b673b34c3f4
David Halvorsen <daviqd.halvorsen@gmail.com>
2017-11-18 19:55:37 -0800

    Update Personal Library.java

--- Learn Java/Personal Library.java
+++ Learn Java/Personal Library.java
@@ -1,10 +1,20 @@
+/*
+This program is called Personal Library. It is from Chapter 4,
+the Data Structures chapter of Codecademy's Learn Java track. 
+This program keeps track of the books that a user has read.
+*/
 import java.util.HashMap;
 import java.lang.Iterable;
-
+//This is the library class.
 public class Library {
   public Library() {
     
   }
+  // getFinishedBooks accepts a HashMap that holds strings 
+  //(of books) and Boolean Values (of whether or not those
+  //books have been read.) This function cycles through the
+  //books in the library keyset and prints out that you've
+  //read the book for all books that have been read.
   public void getFinishedBooks(HashMap<String, Boolean> library) {
     if(library.size() < 1) {
       System.out.println("Your HashMap is empty.");
@@ -16,6 +26,8 @@ public class Library {
       }
     }
   }  
+  //This creates a new library and populates it with books that
+  //have already been read. 
   public static void main(String[] args) {
   	Library myLibrary = new Library();
     HashMap<String, Boolean> myBooks = new HashMap<String, Boolean>();
