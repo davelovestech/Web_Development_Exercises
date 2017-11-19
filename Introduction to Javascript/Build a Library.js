ee85357f7b8109595163cf32cd87320f37a26e73
David Halvorsen <daviqd.halvorsen@gmail.com>
2017-11-18 16:08:56 -0800

    Update Build a Library.js

--- Introduction to Javascript/Build a Library.js
+++ Introduction to Javascript/Build a Library.js
@@ -1,3 +1,25 @@
+/*
+This Codecademy exercise is called Build a Library. It is part
+of the Classes chapter for the Introduction to Javascript
+course. All of the following code was written by me from 
+nothing, BUT it was by following the Codecademy tutorial. 
+This program replicates media inventory system for a library.
+There's a main class called Media that has instances that
+any type of library media would have and then three 
+extension classes with unique sub-types: Book, CD & Movie. 
+*/
+
+/*
+The main class is media. it's constructor accepts an input
+of title and makes an empty array for ratings and gives it a
+false boolean for being checked out. It has methods for 
+getting the title, whether or not it's checked out, ratings, 
+toggling it's checked out status, getting the average rating (
+which uses the get rating method) and also adding ratings. The
+add rating method pushes a new rating to the rating array. The
+get average rating method sums together the ratings and divides
+them by the number of ratings. 
+*/
 class Media {
   constructor(title) {
     this._title = title;
@@ -26,6 +48,14 @@ class Media {
     this.ratings.push(newRating);
   }
 }
+/*
+The Book class extends Media, so it has access to all of the
+Media methods AND it has new methods as well. Its constructor
+accepts parameters for author, title & pages. The super keyword
+is used to access the title functions of the parent Media 
+class. The constructor creates the author and pages instances 
+of the Book object and the get methods return those. 
+*/
 class Book extends Media {
   constructor(author, title, pages) {
     super(title);
@@ -40,6 +70,11 @@ class Book extends Media {
     return this._pages;
   }
 }
+/*
+The movie class is conceptually similar to the Book class with
+the difference of having director, title & runTime instead of
+the book stuff. 
+*/
 class Movie extends Media {
   constructor(director, title, runTime) {
     super(title);
@@ -54,6 +89,9 @@ class Movie extends Media {
     return this._runTime;
   }
 }
+/*
+The CD class is conceptually the same as the previous classes.
+*/
 class CD extends Media {
   constructor(artist, title, songs) {
    super(title); 
@@ -67,22 +105,35 @@ class CD extends Media {
     return this._songs;
   }
 }
+/*
 
-
+*/
+console.log("_______________________________________________")
 const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+console.log("'A Short History of Nearly Everything' was defined right before this console.log as a Book. Here's the call to its checkout status. It will be false because that's how new media objects are created.")
 console.log(historyOfEverything.isCheckedOut);
+console.log("-----------------------------------------------")
 historyOfEverything.toggleCheckOutStatus();
+console.log("The toggleCheckoutStatus was just called on the previously mentioned history book, so it should now print 'True.'")
 console.log(historyOfEverything.isCheckedOut);
+console.log("-----------------------------------------------")
 historyOfEverything.addRating(4);
 historyOfEverything.addRating(5);
 historyOfEverything.addRating(5);
+console.log("Three ratings were just added to the History book and now the getAverageRating is being called, so it should print the average of those ratings here.")
 console.log(historyOfEverything.getAverageRating());
-
+console.log("_______________________________________________")
 const speed = new Movie('Jan de Bont', 'Speed', 116);
+console.log("The movie speed was just created above this print call, so the isCheckedOut function should return false: ")
 console.log(speed.isCheckedOut);
+console.log("-----------------------------------------------")
 speed.toggleCheckOutStatus();
+console.log("The checkOutStatus was just called on the Speed movie object, so the isCheckedOut function should now return 'True.'")
 console.log(speed.isCheckedOut);
+console.log("-----------------------------------------------")
 speed.addRating(1);
 speed.addRating(1);
 speed.addRating(5);
+console.log("Three ratings were just added to the Speed movie, so the getAverageRating function should now display the average rating: ")
 console.log(speed.getAverageRating());
+console.log("_______________________________________________")
