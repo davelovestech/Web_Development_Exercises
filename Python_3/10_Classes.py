c142d840f57b6247b07f90afcae116f3fa72e561
DaveHalvorsen <daviqd.halvorsen@gmail.com>
2019-04-15 11:12:59 -0700

    commit

---
+++ Python_3/10_Classes.py
@@ -0,0 +1,18 @@
+# review int, dict, and list
+print(type(5))
+
+my_dict = {}
+print(type(my_dict))
+
+my_list = []
+print(type(my_list))
+
+# define an empty class
+class Facade:
+  pass
+
+# instantiating a class
+class Facade:
+  pass
+
+facade_1 = Facade()
